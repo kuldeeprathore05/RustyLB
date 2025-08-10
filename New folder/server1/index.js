7bb6ddda9ebd96ff9336b3bc5e7db6f01495e887
Kuldeep <kuldeeprathore5005@gmail.com>
2025-08-10 19:25:27 +0530

    servers

---
+++ New folder/server1/index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express();
+app.use(express.json())
+app.get('/', (req, res) => {
+  console.log('Received POST data:', req.body);
+  res.json({ message: 'Server3 request received', data: req.body });
+});
+
+app.listen(8001, () => {
+  console.log('Server running on http://localhost:8001');
+});
